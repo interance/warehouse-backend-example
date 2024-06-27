db1ed3fe7ea5e3d1f48fc5278ead82374b0fd368
Dominik Charousset <dominik@charousset.de>
2024-06-27 13:56:45 +0200

    Update the example for CAF 1.0

--- warehouse-backend-example/controller_actor.cpp
+++ warehouse-backend-example/controller_actor.cpp
@@ -2,55 +2,35 @@
 
 #include "controller_actor.hpp"
 
+#include "log.hpp"
+
 #include <caf/actor.hpp>
 #include <caf/blocking_actor.hpp>
 #include <caf/blocking_mail.hpp>
 #include <caf/config.hpp>
+#include <caf/event_based_actor.hpp>
+#include <caf/flow/byte.hpp>
+#include <caf/flow/string.hpp>
 #include <caf/json_reader.hpp>
 #include <caf/net/socket.hpp>
 #include <caf/net/tcp_accept_socket.hpp>
 #include <caf/net/tcp_stream_socket.hpp>
+#include <caf/scheduled_actor/flow.hpp>
 #include <caf/typed_actor.hpp>
-
-// Platform-dependent code for `select`
-#ifdef CAF_WINDOWS
-#  include <winsock2.h>
-#else
-#  include <sys/select.h>
-#endif
 
 using namespace std::literals;
 
 namespace {
-
-constexpr size_t max_workers = 3;
-
-constexpr size_t max_buffer_size = 1024; // 1KB
-
-// Function to wait until the socket becomes ready for reading
-bool wait_ready(caf::net::socket_id fd, caf::timespan timeout) {
-  // Initialize the socket set.
-  fd_set readfds;
-  FD_ZERO(&readfds);
-  FD_SET(fd, &readfds);
-  // Set up the timeout
-  using std::chrono::duration_cast;
-  timeval tv;
-  tv.tv_sec = duration_cast<std::chrono::seconds>(timeout).count();
-  tv.tv_usec = duration_cast<std::chrono::microseconds>(timeout).count()
-               % 1'000'000;
-  // Block until the socket is ready for reading or the timeout occurs.
-  int ready = select(fd + 1, &readfds, nullptr, nullptr, &tv);
-  if (ready < 0)
-    throw std::logic_error("Error in select()");
-  return ready > 0;
-}
 
 struct command {
   // Either "inc" or "dec".
   std::string type;
   int32_t id = 0;
   int32_t amount = 0;
+
+  bool valid() const noexcept {
+    return type == "inc" || type == "dec";
+  }
 };
 
 template <class Insepctor>
@@ -59,160 +39,92 @@ bool inspect(Insepctor& f, command& x) {
                             f.field("amount", x.amount));
 }
 
-void worker_impl(caf::blocking_actor* self, database_actor db_actor,
-                 caf::net::tcp_stream_socket fd) {
-  if (auto err = caf::net::nonblocking(fd, true)) {
-    self->fail_state(std::move(err));
-    return;
-  }
-  caf::json_reader reader;
-  auto received = size_t{0};
-  caf::byte_buffer buffer;
-  buffer.resize(max_buffer_size);
-  auto done = false;
-  self->monitor(db_actor);
-  do {
-    // Wait for socket activity, but wake up every 100ms to check the mailbox.
-    auto socket_ready = wait_ready(fd.id, 100ms);
-    // Drain the mailbox. Passing 0s as the timeout basically makes `receive`
-    // non-blocking and calls the timeout handler if no message is available.
-    auto receiving = true;
-    self->receive_while(receiving)(
-      [&](caf::exit_msg& msg) {
-        self->fail_state(std::move(msg.reason));
-        done = true;
-        receiving = false;
-      },
-      [&](caf::down_msg& msg) {
-        if (msg.source == db_actor) {
-          self->fail_state(std::move(msg.reason));
-          done = true;
-          receiving = false;
-        }
-      },
-      caf::after(0s) >>
-        [&] {
-          receiving = false;
-          if (!socket_ready)
-            return;
-          // Try to fill up the buffer.
-          auto res = caf::net::read(fd,
-                                    caf::make_span(buffer).subspan(received));
-          if (res < 0) {
-            if (caf::net::last_socket_error_is_temporary()) {
-              // Simply try again later.
-              return;
-            }
-            self->fail_state(caf::sec::socket_operation_failed);
-            done = true;
-            return;
-          }
-          if (res == 0) {
-            self->fail_state(caf::sec::socket_disconnected);
-            done = true;
-            return;
-          }
-          received += static_cast<size_t>(res);
-          // Look for the last newline character in the buffer.
-          auto bytes = caf::make_span(buffer).subspan(0, received);
-          auto pos = std::find(bytes.rbegin(), bytes.rend(), std::byte{'\n'});
-          if (pos == bytes.rend()) {
-            // Abort if the buffer is full without finding a newline.
-            if (received == max_buffer_size) {
-              self->fail_state(caf::sec::runtime_error);
-              done = true;
-            }
-            return;
-          }
-          // Check whether the input is valid UTF-8.
-          bytes = bytes.subspan(0, bytes.size() - (pos - bytes.rbegin()));
-          if (!caf::is_valid_utf8(bytes)) {
-            self->fail_state(caf::sec::runtime_error);
-            done = true;
-            return;
-          }
-          // Split the input into lines and process each line.
-          std::vector<std::string_view> lines;
-          auto str = caf::to_string_view(bytes);
-          caf::split(lines, str, caf::is_any_of("\n"));
-          for (auto line : lines) {
-            // Parse the line as JSON.
-            if (!reader.load(line)) {
-              self->fail_state(caf::sec::runtime_error);
-              done = true;
-              return;
-            }
-            // Parse the JSON object as a command.
-            command cmd;
-            if (!reader.apply(cmd) || cmd.type != "inc" && cmd.type != "dec") {
-              self->fail_state(caf::sec::runtime_error);
-              done = true;
-              return;
-            }
-            // Send the command to the database actor.
-            if (cmd.type == "inc") {
-              self->mail(inc_atom_v, cmd.id, cmd.amount).send(db_actor);
-            } else {
-              self->mail(dec_atom_v, cmd.id, cmd.amount).send(db_actor);
-            }
-          }
-        });
-  } while (!done);
-}
-
-void controller_impl(caf::blocking_actor* self, database_actor db_actor,
-                     caf::net::tcp_accept_socket fd) {
-  if (auto err = caf::net::nonblocking(fd, true)) {
-    self->fail_state(std::move(err));
-    return;
-  }
-  auto done = false;
-  self->monitor(db_actor);
-  std::set<caf::actor_addr> workers;
-  do {
-    // If we have reached the maximum worker count, we simply wait until a new
-    // message arrives. Otherwise, we wait up to 100ms on the socket before
-    // checking the mailbox.
-    auto socket_ready = false;
-    if (workers.size() >= max_workers)
-      self->await_data();
-    else
-      socket_ready = wait_ready(fd.id, 100ms);
-    // Drain the mailbox. Passing 0s as the timeout basically makes `receive`
-    // non-blocking and calls the timeout handler if no message is available.
-    auto receiving = true;
-    self->receive_while(receiving)(
-      [&](caf::exit_msg& msg) {
-        self->fail_state(std::move(msg.reason));
-        done = true;
-        receiving = false;
-      },
-      [&](caf::down_msg& msg) {
-        if (msg.source == db_actor) {
-          self->fail_state(std::move(msg.reason));
-          done = true;
-          receiving = false;
-          return;
-        }
-        workers.erase(msg.source);
-      },
-      caf::after(0s) >>
-        [&] {
-          receiving = false;
-          if (!socket_ready)
-            return;
-          if (auto conn_fd = caf::net::accept(fd)) {
-            auto worker = self->spawn(worker_impl, db_actor, *conn_fd);
-            workers.insert(worker.address());
-          }
-        });
-  } while (!done);
-}
-
 } // namespace
 
-caf::actor spawn_controller_actor(caf::actor_system& sys,
-                                  database_actor db_actor,
-                                  caf::net::tcp_accept_socket fd) {
-  return sys.spawn(controller_impl, db_actor, fd);
+caf::actor
+spawn_controller_actor(caf::actor_system& sys, database_actor db_actor,
+                       caf::net::acceptor_resource<std::byte> events){
+  return sys
+    .spawn(
+      [events,db_actor](caf::event_based_actor* self) mutable {
+      // Stop if the database actor terminates.
+      self->monitor(db_actor, [self](const caf::error& reason) {
+        log::info("controller lost the database actor: {}", reason);
+        self->quit(reason);
+      });
+      // For each buffer pair, we create a new flow ...
+      events.observe_on(self)
+        .for_each(
+          [self, db_actor](auto ev) {
+            log::info("controller added a new client");
+            auto [pull, push] = ev.data();
+            pull
+              .observe_on(self)
+              // ... that converts the lines to commands ...
+              .transform(caf::flow::byte::split_as_utf8_at('\n'))
+              .map([self](const caf::cow_string& line) {
+                log::debug("controller received line: {}", line.str());
+                caf::json_reader reader;
+                if (!reader.load(line.str())) {
+                  log::error("controller failed to parse JSON: {}",
+                             reader.get_error());
+                  return std::shared_ptr<command>{}; // Invalid JSON.
+                }
+                auto ptr = std::make_shared<command>();
+                if (!reader.apply(*ptr))
+                  return std::shared_ptr<command>{}; // Not a command.
+                return ptr;
+              })
+              .concat_map(
+                [self, db_actor](std::shared_ptr<command> ptr) {
+                  // If the `map` step failed, inject an error message.
+                  if (ptr == nullptr || !ptr->valid()) {
+                    auto str = R"_({"error":"invalid command"})_"s;
+                    return self->make_observable()
+                      .just(caf::cow_string{std::move(str)})
+                      .as_observable();
+                  }
+                  // Send the command to the database actor and convert the
+                  // result message into an observable.
+                  caf::flow::observable<int32_t> result;
+                  if (ptr->type == "inc") {
+                    result = self->mail(inc_atom_v, ptr->id, ptr->amount)
+                               .request(db_actor, 1s)
+                               .as_observable();
+                  } else {
+                    result = self->mail(dec_atom_v, ptr->id, ptr->amount)
+                               .request(db_actor, 1s)
+                               .as_observable();
+                  }
+                  // On error, we return an error message to the client.
+                  return result
+                    .map([ptr](int32_t res) {
+                      log::debug("controller received result for {} -> {}",
+                                 *ptr, res);
+                      auto str = R"_({"result":)_"s;
+                      str += std::to_string(res);
+                      str += '}';
+                      return caf::cow_string{std::move(str)};
+                    })
+                    .on_error_return([ptr](const caf::error& what) {
+                      log::debug("controller received an error for {} -> {}",
+                                 *ptr, what);
+                      auto str = R"_({"error":")_"s;
+                      str += to_string(what);
+                      str += R"_("})_";
+                      auto res = caf::cow_string{std::move(str)};
+                      return caf::expected<caf::cow_string>{std::move(res)};
+                    })
+                    .as_observable();
+                })
+              // ... disconnects if the client is too slow ...
+              .on_backpressure_buffer(32)
+              // ... and pushes the results back to the client as bytes.
+              .transform(caf::flow::string::to_chars("\n"))
+              .do_finally(
+                [] { log::info("controller lost connection to a client"); })
+              .map([](char ch) { return static_cast<std::byte>(ch); })
+              .subscribe(push);
+          });
+      });
 }
